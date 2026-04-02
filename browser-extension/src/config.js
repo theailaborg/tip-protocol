9693f5c38f35f76ad9dab93046b931735182255c
Tushar Bhendarkar <t.bhendarkar@neuriteai.com>
2026-04-02 18:09:52 +0530

    chore: build prod extension zip with rpId theailab.org & update plugin zip

--- browser-extension/src/config.js
+++ browser-extension/src/config.js
@@ -4,4 +4,4 @@
  */
 
 export const TIP_NODE_URL       = "https://node.theailab.org";
-export const TIP_WEBAUTHN_RP_ID = "localhost";  // prod: "usetip.net"
+export const TIP_WEBAUTHN_RP_ID = "localhost";  // prod: "theailab.org"
